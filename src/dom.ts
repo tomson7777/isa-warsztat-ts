3be8850759c44621db9c377915343b2fa6ab1c76
tomasznastaly <nastalytomasz@gmail.com>
2018-04-23 23:55:15 +0200

    fixing tabs

--- src/dom.ts
+++ src/dom.ts
@@ -4,17 +4,17 @@ export class DOM {
 
 		const heroesNodes = heroes.map(hero => {
 			return `
-        <div class="col-sm-3">
-          <div class="card shadow">
-            <img src="${hero.imageUrl}" class="card-img-top">
-            <div class="card-body">
-              <h5 class="card-title">${hero.name}</h5>
-              <p class="card-text">
-                ${hero.description ? hero.description : 'description not available'}
-              </p>
-            </div>
-          </div>
-        </div>`
+				<div class="col-sm-3">
+					<div class="card shadow">
+					<img src="${hero.imageUrl}" class="card-img-top">
+					<div class="card-body">
+					  <h5 class="card-title">${hero.name}</h5>
+					  <p class="card-text">
+						${hero.description ? hero.description : 'description not available'}
+					  </p>
+					</div>
+				  </div>
+				</div>`
 		});
 
 		heroesContainer.innerHTML = heroesNodes.join('');
